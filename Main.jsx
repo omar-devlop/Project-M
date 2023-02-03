cd2801ef62d57a6e10a13db79e4f8a15a088e748
OMARALAFA <omaralafadouma@gmail.com>
2023-02-03 03:26:21 +0300

    Create basic translation system

--- Main.jsx
+++ Main.jsx
@@ -1,11 +1,26 @@
 //@include "modules/readASE.jsx";
+//@include "modules/translations.jsx";
+
+
+var language = "ar";
+
+function getTranslation (key){
+
+    return translations[language][key];
+
+}
+
+
+
 
 (function (thisObj) {
 
     var mainScriptFile = new File($.fileName);
 
-    alert(mainScriptFile.toString());
+    // alert(mainScriptFile.toString());
 
-    alert(readASE());
+    // alert(readASE());
+
+    alert(getTranslation("Exit"));
 
 })(this);
