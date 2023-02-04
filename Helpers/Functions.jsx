c758353a8c9bc9f0d4d144310f2c37fd77a48184
OMARALAFA <omaralafadouma@gmail.com>
2023-02-05 01:23:44 +0300

    Update UI

--- Helpers/Functions.jsx
+++ Helpers/Functions.jsx
@@ -1,6 +1,15 @@
 //@include "Helpers/Setting.jsx";
 
-function getTranslation (key){
+function canWrite() {
+    if (app.preferences.getPrefAsLong("Main Pref Section", "Pref_SCRIPTING_FILE_NETWORK_SECURITY")) {
+        return true;
+    }
+    alert(scriptName + ' requires access to write files.\n' + 'To allow, please go to Preferences check off "Allow Scripts to Write Files and Access Network" to resolve.');
+    return false;
+}
+
+
+function getTranslation(key) {
 
     return translations[language][key];
 
