5c36ee60714dc6b21e35674b945019c7dc341fb4
OMARALAFA <omaralafadouma@gmail.com>
2023-02-26 17:40:31 +0300

    Some Updates

--- modules/readASE.jsx
+++ modules/readASE.jsx
@@ -7,6 +7,7 @@
         // if (data.values[0].type = "CMYK") {
         //     alert(data.values[0].type.toString() + "\n" + data.values[0].c.toString() + "\n" + data.values[0].m.toString() + "\n" + data.values[0].y.toString() + "\n" + data.values[0].k.toString());
         // }
+        
         return data.values[0].type.toString() + "\n" + data.values[0].c.toString() + "\n" + data.values[0].m.toString() + "\n" + data.values[0].y.toString() + "\n" + data.values[0].k.toString()
     }
 }
